e8279b6701628d84cfa9c2feda2e7224c26c2a15
hamedur-rahiman <hamedur.rahiman@gmail.com>
2020-02-26 14:53:06 +0530

    Updated Values

--- SimpleAPI/Controllers/ValuesController.cs
+++ SimpleAPI/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "HR1", "value2" };
+            return new string[] { "HR1", "HR2" };
         }
 
         // GET api/values/5
